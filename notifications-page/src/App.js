6ff8b55c5b7f7ff453c00da92dddff283d72b138
Reshawn Hamilton <reshawnhamilton97@gmail.com>
2022-10-14 06:05:01 -0400

    layout setup done

---
+++ notifications-page/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <div className="full-notification-page">
+      <div className="notification-counter"></div>
+      <div className="notifications-container">
+
+      </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ notifications-page/src/components/Notification.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Notification = () => {
+  return (
+    <div>
+      <img src="" alt="" />
+      <div className="notification-content"></div>
+      <div className="time-elapsed"></div>
+    </div>
+  )
+}
+
+export default Notification
