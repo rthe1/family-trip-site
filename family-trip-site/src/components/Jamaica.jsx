df824ce6b2e28d98bbec31c9bda154d3d1c0cc80
Reshawn Hamilton <reshawnhamilton97@gmail.com>
2022-11-05 04:12:26 -0400

    Home Page added

--- family-trip-site/src/components/Jamaica.jsx
+++ family-trip-site/src/components/Jamaica.jsx
@@ -1,6 +1,14 @@
+import axios from 'axios';
 import React from 'react'
 
 const Jamaica = () => {
+
+  const url = "https://api.open-meteo.com/v1/forecast?latitude=18.10&longitude=77.29&hourly=temperature_2m"
+
+  const weather = axios.get(url, (req,res) => {
+    
+  })
+
   return (
     <div>Jamaica</div>
   )
